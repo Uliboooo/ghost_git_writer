10366abb76ad4c19f176b57eff0e321ba58f2af5
uliboooo <uliboulibo@gmail.com>
2025-07-07 16:37:54 +0900

    fix prompt fnc

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,6 +1,7 @@
-use crate::{llm, Error};
+use crate::{config::ModelInfo, llm, Error};
 
-pub fn cmt_msg(diff: String, prompt: Option<String>) -> Result<String, Error> {
+/// if prompt is None, use default prompt.
+pub fn cmt_msg(diff: String, prompt: Option<String>, model_info: ModelInfo) -> Result<String, Error> {
     let prmt = prompt.unwrap_or(format!("write a git commit message for this diff. \ndiff: {}", diff));
-    llm::call_llms(prmt, service, api_key, temperature, max_tokens)
+    llm::call_llms(prmt, model_info).map_err(Error::llm)
 }
