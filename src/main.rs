8900b2a8c85998464158494a272f441eddf4973c
uliboooo <uliboulibo@gmail.com>
2025-07-08 10:54:39 +0900

    now use git.rs fn in commit_ctrl()

--- src/main.rs
+++ src/main.rs
@@ -48,25 +48,30 @@ struct Readme {}
 #[derive(Debug, clap::Args)]
 struct Sum {}
 
-fn commit_ctrl(_cmt: Commit, repo: git2::Repository) -> Result<String, Error> {
-    let head_commit = repo
-        .head()
-        .map_err(Error::git2)?
-        .peel_to_commit()
-        .map_err(Error::git2)?;
-    let head_tree = head_commit.tree().map_err(Error::git2)?;
-    let diff = repo
-        .diff_tree_to_workdir(Some(&head_tree), Some(&mut DiffOptions::new()))
-        .map_err(Error::git2)?;
-    let mut pa = String::new();
-    diff.print(git2::DiffFormat::Patch, |_, _, line| {
-        if let Ok(t) = std::str::from_utf8(line.content()) {
-            pa.push_str(t);
-        }
-        true
-    })
-    .map_err(Error::git2)?;
-    Ok(pa)
+#[derive(Debug, clap::Args)]
+struct Chat {}
+
+fn commit_ctrl(_cmt: Commit, model: ModelInfo) -> Result<String, Error> {
+    // let head_commit = repo
+    //     .head()
+    //     .map_err(Error::git2)?
+    //     .peel_to_commit()
+    //     .map_err(Error::git2)?;
+    // let head_tree = head_commit.tree().map_err(Error::git2)?;
+    // let diff = repo
+    //     .diff_tree_to_workdir(Some(&head_tree), Some(&mut DiffOptions::new()))
+    //     .map_err(Error::git2)?;
+    // let mut pa = String::new();
+    // diff.print(git2::DiffFormat::Patch, |_, _, line| {
+    //     if let Ok(t) = std::str::from_utf8(line.content()) {
+    //         pa.push_str(t);
+    //     }
+    //     true
+    // })
+    // .map_err(Error::git2)?;
+    let diff = git::get_diff("")?;
+    let prompt = format!("write a git commit comment for this diff: {diff}");
+    llm::call_llms(prompt, model).map_err(Error::llm)
 }
 
 fn create_readme(_rmd: Readme) -> Result<String, Error> {
@@ -84,11 +89,12 @@ fn main() -> Result<(), Error> {
     let repo = Repository::open("").unwrap();
 
     let res = match cli.subcommand {
-        Commands::Cmt(commit) => commit_ctrl(commit, repo),
+        Commands::Cmt(commit) => commit_ctrl(commit, ModelInfo),
         Commands::Rdm(readme_f) => create_readme(readme_f),
         Commands::Sum(sum) => todo!(),
+        Commands::Chat(chat) => todo!(),
     }?;
 
-    println!("{}", res);
+    println!("{res}");
     Ok(())
 }
